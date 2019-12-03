f6d545aba5d42df2eb7a126ba382d0a604e0f335
Federico Nicolás Flores <flores.federico.nicolas@gmail.com>
2019-12-03 16:09:56 -0300

    ADD: Minify and concat css in a single file

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 let gulp = require('gulp');
 let cleanCSS = require('gulp-clean-css');
+let concat = require('gulp-concat');
 
 exports.default = gulp.task('minify-css', () => {
   return gulp.src('src/css/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
+  	.pipe(concat('style.css'))
+    .pipe(cleanCSS({compatibility: 'ie10+'}))
     .pipe(gulp.dest('css'));
 });
